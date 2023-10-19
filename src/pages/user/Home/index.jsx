6a3b085384d64a448f9c92b2d4a128bc53c5a118
Le Ba Thanh Tuan <le.ba.thanh.tuan@sun-asterisk.com>
2023-10-19 18:49:35 +0700

    Test commit 1

--- src/pages/user/Home/index.jsx
+++ src/pages/user/Home/index.jsx
@@ -1,7 +1,8 @@
 import * as S from "./styles";
+import { Button } from "antd";
 
 function HomePage() {
-  return <S.HomeWrapper>Home Page</S.HomeWrapper>;
+  return <S.HomeWrapper>Home Page ABC</S.HomeWrapper>;
 }
 
 export default HomePage;
--- src/pages/user/Home/styles.jsx
+++ src/pages/user/Home/styles.jsx
@@ -2,4 +2,5 @@ import styled from "styled-components";
 
 export const HomeWrapper = styled.div`
   padding: 16px;
+  background-color: gray;
 `;
